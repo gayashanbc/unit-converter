55b6d1f82ffdb12fe6329ba4d1c1dd2bbfbe9f70
Gayashan Bombuwala <gayashanbc@gmail.com>
2019-03-16 22:57:42 +0530

    Create Converter Skeleton

--- Unit Converter/ConverterViewController.swift
+++ Unit Converter/ConverterViewController.swift
@@ -10,11 +10,46 @@ import UIKit
 
 class ConverterViewController: UIViewController {
 
+    @IBOutlet weak var contentView: UIView!
+    @IBOutlet var tabBarButtons: [UIButton]!
+    
+    var weightViewController: UIViewController!
+    var tabBarViewControllers: [UIViewController]!
+    var selectedIndex = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        weightViewController = storyboard.instantiateViewController(withIdentifier: "WeightViewController")
+        tabBarViewControllers = [weightViewController]
+        
+        tabBarButtons[selectedIndex].isSelected = true
+        didPressTab(tabBarButtons[selectedIndex])
     }
 
-
+    @IBAction func didPressTab(_ sender: UIButton) {
+        let previousIndex = selectedIndex
+        selectedIndex = sender.tag
+        
+        tabBarButtons[previousIndex].isSelected = false
+        
+        let previousViewController = tabBarViewControllers[previousIndex]
+        
+        previousViewController.willMove(toParent: nil)
+        previousViewController.view.removeFromSuperview()
+        previousViewController.removeFromParent()
+        
+        sender.isSelected = true
+        
+        let newViewController = tabBarViewControllers[selectedIndex]
+        
+        addChild(newViewController)
+        
+        newViewController.view.frame = contentView.bounds
+        contentView.addSubview(newViewController.view)
+        
+        newViewController.didMove(toParent: self)
+    }
+    
 }
 
---
+++ Unit Converter/WeightsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  WeightsViewController.swift
+//  Unit Converter
+//
+//  Created by Gayashan Bombuwala on 3/16/19.
+//  Copyright © 2019 Gayashan Bombuwala. All rights reserved.
+//
+
+import UIKit
+
+class WeightsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
