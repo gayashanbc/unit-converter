96695ad4ed982edf807817dcc5acbeb054068f4f
Gayashan Bombuwala <gayashanbc@gmail.com>
2019-03-23 19:42:39 +0530

    Implement the foundation of the ConverterViewController

--- Unit Converter/ConverterViewController.swift
+++ Unit Converter/ConverterViewController.swift
@@ -8,25 +8,41 @@
 
 import UIKit
 
-class ConverterViewController: UIViewController {
+enum KeyboardButton: Int {
+    case zero, one, two, three, four, five, six, seven, eight, nine, period, delete, negation
+}
 
+class ConverterViewController: UIViewController, UITextFieldDelegate {
+    
     @IBOutlet weak var contentView: UIView!
     @IBOutlet var tabBarButtons: [UIButton]!
+    @IBOutlet weak var negateButton: UIButton!
     
     var weightViewController: UIViewController!
     var tabBarViewControllers: [UIViewController]!
     var selectedIndex = 0
+    var activeTextField = UITextField()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        weightViewController = storyboard.instantiateViewController(withIdentifier: "WeightViewController")
-        tabBarViewControllers = [weightViewController]
-        
-        tabBarButtons[selectedIndex].isSelected = true
+        if let weightViewController = storyboard.instantiateViewController(withIdentifier: "WeightViewController") as? WeightsViewController {
+            weightViewController.parentControllerReference = self
+            tabBarViewControllers = [weightViewController]
+            
+            tabBarButtons[selectedIndex].isSelected = true
+        }
+        //        weightViewController = storyboard.instantiateViewController(withIdentifier: "WeightViewController")
+        //
+        //        tabBarViewControllers = [weightViewController]
+        //
+        //        tabBarButtons[selectedIndex].isSelected = true
+        if selectedIndex == 0 { // TODO chech if temperature is selected
+            negateButton.isEnabled = false
+        }
         didPressTab(tabBarButtons[selectedIndex])
     }
-
+    
     @IBAction func didPressTab(_ sender: UIButton) {
         let previousIndex = selectedIndex
         selectedIndex = sender.tag
@@ -50,6 +66,64 @@ class ConverterViewController: UIViewController {
         
         newViewController.didMove(toParent: self)
     }
+    
+    @IBAction func keyboardButtonPressed(_ sender: UIButton) {
+        print(sender.tag)
+        setKeyValueToTextField(pressedButton: KeyboardButton(rawValue: sender.tag)!)
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        self.activeTextField = textField
+    }
+    
+    func setKeyValueToTextField(pressedButton: KeyboardButton) {
+        let cursorPosition = getCursorPositions()
+        
+        if var currentText = self.activeTextField.text {
+            switch pressedButton {
+            case .delete:
+                if(currentText.count > 0){ // TODO: add to enum
+                    currentText.remove(at: currentText.index(currentText.startIndex, offsetBy: cursorPosition.current - 1))
+                    self.activeTextField.text = currentText
+                }
+            case .negation:
+                currentText.insert("-", at: currentText.index(currentText.startIndex, offsetBy: 0))
+                self.activeTextField.text = currentText
+            case .period:
+                if(!currentText.contains(".")){
+                    activeTextField.insertText(".")
+                    if(currentText.count > 1){ // TODO: add to enum
+                        activeTextField.selectedTextRange = activeTextField.textRange(from: cursorPosition.next, to: cursorPosition.next)
+                    }
+                }
+            default:
+                activeTextField.insertText(String(pressedButton.rawValue))
+                if(currentText.count > 1){ // TODO: add to enum
+                    activeTextField.selectedTextRange = activeTextField.textRange(from: cursorPosition.next, to: cursorPosition.next)
+                }
+            }
+            return;
+        }
+    }
+    
+    func getCursorPositions() -> (current: Int, next: UITextPosition)  {
+        var currentPosition = 0
+        var nextPosition = activeTextField.beginningOfDocument
+        
+        if let selectedRange = activeTextField.selectedTextRange {
+            currentPosition = activeTextField.offset(from: activeTextField.beginningOfDocument, to: selectedRange.start)
+            
+            if let position = activeTextField.position(from: selectedRange.start, offset: +1) {
+                nextPosition = position
+            }
+            
+            print("Current cursor position: \(currentPosition)")
+            print("Next cursor position: \(nextPosition)")
+        }
+        
+        return (currentPosition, nextPosition)
+    }
+    
     
 }
 
--- Unit Converter/WeightsViewController.swift
+++ Unit Converter/WeightsViewController.swift
@@ -8,23 +8,82 @@
 
 import UIKit
 
-class WeightsViewController: UIViewController {
+enum WeightMetrics: Int {
+    case ounce, pound, gram, stone, kg
+    
+    func getConversionRateToKg() -> Double {
+        switch self {
+        case .ounce:
+            return 0.02834952
+        case .pound:
+            return 0.4535923
+        case .gram:
+            return 0.001000000
+        case .stone:
+            return 6.350293
+        default:
+            return 1
+        }
+    }
+}
 
+class WeightsViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var ounceTextField: UITextField!
+    @IBOutlet weak var poundTextField: UITextField!
+    @IBOutlet weak var gramTextField: UITextField!
+    @IBOutlet weak var stoneTextField: UITextField!
+    @IBOutlet weak var kgTextField: UITextField!
+    
+    var textFields: [UITextField]!
+    
+    var parentControllerReference: UITextFieldDelegate?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        ounceTextField.delegate = parentControllerReference
+        poundTextField.delegate = parentControllerReference
+        gramTextField.delegate = parentControllerReference
+        stoneTextField.delegate = parentControllerReference
+        kgTextField.delegate = parentControllerReference
+        
+        textFields = [ounceTextField, poundTextField, gramTextField, stoneTextField, kgTextField]
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func textFieldEditingChanged(_ sender: UITextField) {
+        print("Eidted \(sender.tag)")
+        print("Conversin rate: \(WeightMetrics.ounce.getConversionRateToKg())")
+        
+        guard let textFieldValue = sender.text else {return}
+        guard let enteredNumValue = Double(textFieldValue) else {return}
+        guard let baseConversionRate = WeightMetrics(rawValue: sender.tag)?.getConversionRateToKg() else {return}
+        
+        let enteredValueInKg = enteredNumValue * baseConversionRate
+        print("Kg Value: \(enteredValueInKg)")
+        
+        for textField in textFields {
+            if let conversionRateToKg = WeightMetrics(rawValue: textField.tag)?.getConversionRateToKg(){
+                textField.text = roundValue(enteredValueInKg / conversionRateToKg)
+            }
+        }
     }
-    */
-
+    
+    func roundValue(_ valueToRound: Double) -> String {
+        var roundedValue = String(round(10000 * valueToRound) / 10000)
+        if let trailingZeros = roundedValue.components(separatedBy: ".").last?.count {
+            switch trailingZeros {
+            case 1:
+                roundedValue += "000"
+            case 2:
+                roundedValue += "00"
+            case 3:
+                roundedValue += "0"
+            default:
+                break
+            }
+        }
+        return roundedValue
+    }
+    
+    
 }
